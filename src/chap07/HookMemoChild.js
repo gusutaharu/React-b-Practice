31b1ddbc763a720a32703197165f43fd644b4040
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2025-02-19 09:48:12 +0900

    memo関数でコンポーネントのメモ化

--- src/chap07/HookMemoChild.js
+++ src/chap07/HookMemoChild.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { memo } from 'react'
 
-export const MyButton = ({ id, handleClick, children}) => {
+export const MyButton = memo(({ id, handleClick, children}) => {
   console.log(`MyButton is called: ${id}`);
   return(
     <button onClick={handleClick}>{children}</button>
   );
-};
+});
 
-export const MyCounter = ({ id, value }) => {
+export const MyCounter = memo(({ id, value }) => {
   console.log(`MyCounter is called: ${id}`);
   return(
     <p>現在値:{value}</p>
   )
-}
+});
